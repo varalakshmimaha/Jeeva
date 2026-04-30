bf1568c4244e7a23c0fd70eabd6fa702722f55ad
varalakshmimaha <varalakshmimaha534@gmail.com>
2026-04-30 21:33:57 +0530

    Push My Why description down to neck level

--- laravel-app/resources/views/pages/about.blade.php
+++ laravel-app/resources/views/pages/about.blade.php
@@ -959,7 +959,7 @@
       font-size: clamp(18px, 2vw, 22px);
       color: rgba(255,255,255,0.95);
       line-height: 1.6;
-      margin: 30px 0 0;
+      margin: 130px 0 0;
       max-width: 700px;
       font-weight: 500;
     }
